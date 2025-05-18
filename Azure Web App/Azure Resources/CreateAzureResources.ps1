549e08aba9c014033f70aa6d1f652cc81f503a7c
nickngarcia <152123308+nickngarcia@users.noreply.github.com>
2025-05-17 21:51:11 -0700

    Added Step 2 to CreateAzureResources.ps1

--- Azure Web App/Azure Resources/CreateAzureResources.ps1
+++ Azure Web App/Azure Resources/CreateAzureResources.ps1
@@ -1,1 +1,7 @@
+# Step 1: Description: This script creates an Azure Resource Group for a Web App.
 New-AzureRmResourceGroup -Name "NicksWebAppPS" -Location "West US 2"
+
+# Step 2: Description: This script creates an Azure Web App and Service Plan.
+New-AzAppServicePlan -Name "NicksWebAppPS" -Location "West US" -ResourceGroupName "NicksWebAppPS" -Tier "Free"
+
+#Step 3: Description: Create a web app within the service plan.
