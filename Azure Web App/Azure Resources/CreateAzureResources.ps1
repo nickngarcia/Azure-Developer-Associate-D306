722bd13d374f92022224136de7e0799b72b20afd
nickngarcia <152123308+nickngarcia@users.noreply.github.com>
2025-05-19 18:50:25 -0700

    New Comit

--- Azure Web App/Azure Resources/CreateAzureResources.ps1
+++ Azure Web App/Azure Resources/CreateAzureResources.ps1
@@ -3,5 +3,3 @@ New-AzureRmResourceGroup -Name "NicksWebAppPS" -Location "West US 2"
 
 # Step 2: Description: This script creates an Azure Web App and Service Plan.
 New-AzAppServicePlan -Name "NicksWebAppPS" -Location "West US" -ResourceGroupName "NicksWebAppPS" -Tier "Free"
-
-#Step 3: Description: Create a web app within the service plan.
